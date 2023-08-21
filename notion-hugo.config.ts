1d31985c302200fd09b98c0065eed609e6c05ea2
MevinFernando <42856943+MevinFernando@users.noreply.github.com>
2023-08-21 13:09:33 +0530

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://safe-layer-1cc.notion.site/Notion-DoIt-c70b8246c6574655bfa121646b169964?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
